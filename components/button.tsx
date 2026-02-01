402797880186ba71ecc86b2f0d3aad0c05f5b7ee
nomix21 <tangn14@mcmaster.ca>
2026-01-31 19:51:34 -0500

    login formatting updates

---
+++ components/button.tsx
@@ -0,0 +1,53 @@
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+type RootStackParamList = {
+  insights: undefined;
+};
+
+type Props = {
+  label: string;
+};
+
+export default function Button({ label }: Props) {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate("insights")} // screen is insights.tsx
+      >
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    height: 48,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#ff8fb1",
+    width: "100%", // <- fill the container width
+    height: "100%", // <- optional, fill container height
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 24,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonLabel: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 16,
+    justifyContent: "center",
+  },
+});
