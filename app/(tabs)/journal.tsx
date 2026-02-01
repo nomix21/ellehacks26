c0f685a1f768f69fb73620f9f73d220ff5fea092
emtato <emiliama2006@gmail.com>
2026-01-31 23:26:55 -0500

    useridtest

--- app/(tabs)/journal.tsx
+++ app/(tabs)/journal.tsx
@@ -14,6 +14,7 @@ import {
     View,
 } from "react-native";
 import { saveJournalEntry } from '@/api/call_backend';
+import {useSession} from "@/app/context/SessionContext";
 
 export default function Journal() {
     //questionnaire stuff
@@ -30,11 +31,11 @@ export default function Journal() {
     const [journalContent, setJournalContent] = useState<string>("");
 
   const emojis = ["😄", "🙂", "😐", "😞", "😢"];
-
+  const {userId} = useSession()
   const buildQuizPayload = useCallback(() => {
     return {
       date: new Date().toISOString(),
-      user_ID: "",
+      user_ID: userId,
       tomorrow: tomorrowGoal,
       yesterday_goal: metYesterdayGoal,
       quiz: {
