cf7ba05bfb3b164c445d031c4b25bfd4a24dcdd6
nomix21 <tangn14@mcmaster.ca>
2026-01-31 20:10:42 -0500

    sign in button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,61 +1,71 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#fff",
-        headerStyle: { backgroundColor: "#1b1b1b" },
-        headerShadowVisible: true,
-        headerTintColor: "#fff",
-        tabBarStyle: { backgroundColor: "#1b1b1b" },
-        tabBarInactiveTintColor: "#cccbcb",
-      }}
-    >
-      
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: " ",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
-          ),
+    <View style={styles.darkBackground}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#fff",
+          headerStyle: { backgroundColor: "#1b1b1b" },
+          headerShadowVisible: true,
+          headerTintColor: "#fff",
+          tabBarStyle: { backgroundColor: "#1b1b1b" },
+          tabBarInactiveTintColor: "#cccbcb",
         }}
-      />
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: " ",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "home-sharp" : "home-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="login"
-        options={{
-          title: " ",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "person-circle-outline" : "person-circle-outline"}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      
-      <Tabs.Screen
-        name="insights"
-        options={{
-          title: " ",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "mail-outline" : "mail-outline"}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="login"
+          options={{
+            title: " ",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={
+                  focused ? "person-circle-outline" : "person-circle-outline"
+                }
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
 
-    </Tabs>
+        <Tabs.Screen
+          name="insights"
+          options={{
+            title: " ",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "mail-outline" : "mail-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  darkBackground: {
+    flex: 1,
+    backgroundColor: "#1B1B1B",
+  },
+});
